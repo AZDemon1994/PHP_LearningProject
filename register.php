27678147dc5dbfcf17be61354a7a6f2c4f02257b
AZDemon1994 <zayaraung.blogger@gmail.com>
2022-10-15 18:46:22 +0630

    day 1

--- register.php
+++ register.php
@@ -1,5 +1,36 @@
 <?php include "includes/header.php"; ?>
 
-<h4 class="text-center p-5">This is registar page.</h4>
+<div class="container">
+  <div class="card mx-auto" id="register-card">
+    <div class="card-header">
+      <div class="card-title">
+        <h4>User Register</h4>
+      </div>
+    </div>
+    <div class="card-body">
+      <form>
+        <div class="form-group mb-3">
+          <input type="text" placeholder="Phone Number" class="form-control">
+        </div>
+        <div class="form-group mb-3">
+          <input type="password" placeholder="Password" class="form-control">
+        </div>
+        <div class="form-group mb-3">
+          <input type="password" placeholder="Confirm Password" class="form-control">
+        </div>
+        <!-- <div class="form-group mb-3">
+          <select class="form-control">
+            <option selected>Chose Account Type</option>
+            <option>Bookie</option>
+            <option>Agent</option>
+            <option>User</option>
+          </select>
+        </div> -->
+        <button class="btn btn-success" type="button" name="button">Register</button>
+      </form>
+    </div>
+  </div>
+</div>
+
 
 <?php include "includes/footer.php"; ?>
